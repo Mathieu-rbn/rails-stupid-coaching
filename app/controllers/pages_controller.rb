e5da92b65c3f810cb25a8b39c1fdab796c226499
Mathieu Robin <mathieu.robin1@gmail.com>
2019-08-18 14:42:18 +0200

    done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,25 @@
 class PagesController < ApplicationController
   def questions
+    # raise
     # method to be written
+    # @questions = questions
   end
 
   def answers
+    answers = params[:question]
+    if answers.downcase == "i am going to work right now!"
+      return ""
+    elsif answers.include? "?"
+      return "Silly question, get dressed and go to work!"
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+  end
+
+  def home
     # method to be written
   end
 end
+
+# how to go from Controller to View
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/questions', to: 'pages#questions'
   get '/answers', to: 'pages#answers'
+
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
